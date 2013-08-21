ba6d49c8f636068f47ef31d699bc51d811885eed
Stefan Pfeiffer <pfeiffer@inf.fu-berlin.de>
2013-08-21 14:28:02 +0200

    impl stm32f407vg uart

--- stm32f4discovery/board_init.c
+++ stm32f4discovery/board_init.c
@@ -112,10 +112,13 @@
   ******************************************************************************
   */
 
+#include "board-conf.h"
+#include "stm32f407vg-uart.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_usart.h"
 #include "stm32f4xx_rcc.h"
 #include "stm32f4xx_gpio.h"
+
 
 #define VECT_TAB_OFFSET  0x00 /*!< Vector Table base offset field. 
                                    This value must be a multiple of 0x200. */
@@ -173,38 +176,7 @@ void SystemInit(void)
 
   SCB->VTOR = FLASH_BASE | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal FLASH */
 
-	GPIO_InitTypeDef GPIO_InitStructure;
-	USART_InitTypeDef USART_InitStructure;
-
-	/* enable peripheral clock for USART2 */
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
-
-
-	/* GPIOC clock enable */
-	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
-
-	/* GPIOA Configuration:  USART6 TX,RX on PC06,PC07 */
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6 | GPIO_Pin_7;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
-	GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP ;
-	GPIO_Init(GPIOC, &GPIO_InitStructure);
-
-	/* Connect USART6 pins to AF2 */
-	// TX = PC06, RX = PC07
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART6);
-	GPIO_PinAFConfig(GPIOC, GPIO_PinSource7, GPIO_AF_USART6);
-
-	USART_InitStructure.USART_BaudRate = 115200;
-	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-	USART_InitStructure.USART_StopBits = USART_StopBits_1;
-	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-	USART_InitStructure.USART_Mode = (USART_Mode_Tx|USART_Mode_Rx);
-	USART_Init(USART6, &USART_InitStructure);
-
-	USART_Cmd(USART6, ENABLE); // enable USART6
+  uart_init(USART2);
 }
 
 /**
