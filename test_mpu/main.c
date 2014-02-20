7590175069fa2d90e3bb3f84119b6e821ce08120
Tobias Famulla <uni@famulla.eu>
2014-02-20 18:32:06 +0100

    Anpassung an neuen TCB

--- test_mpu/main.c
+++ test_mpu/main.c
@@ -8,13 +8,13 @@ int main(void)
 	volatile tcb_t *thread = active_thread;
     puts("Hello World!\n");
     printf("Start Address of thread: %#010x\n", thread);
-    printf("Start Address of stack: %#010x\n", thread->stack_start);
+    printf("Start Address of stack: %#010x\n", thread->memory->start_address);
     printf("Size of the Stack: %d\n", thread->stack_size );
     printf("Controll-Register: %#010x\n", __get_CONTROL());
 
     uint32_t writesize = 64;
     uint8_t *writepoint;
-    writepoint = thread->stack_start;
+    writepoint = thread->memory->start_address;
 	enable_unprivileged_mode();
     for (uint32_t i=0; i<= writesize; i++) {
     	*writepoint = 0xAA;
