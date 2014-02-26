066108e0abc4a48d7bf8c671bb89c1d503d9a2dd
Tobias Famulla <uni@famulla.eu>
2014-02-26 07:22:48 +0100

    Staktest eingefügt

--- test_mpu/main.c
+++ test_mpu/main.c
@@ -2,6 +2,9 @@
 #include <sched.h>
 #include <tcb.h>
 #include <memmgmt.h>
+#include <stddef.h>
+#include <cpu.h>
+#include <mpu.h>
 
 int main(void)
 {
@@ -12,14 +15,30 @@ int main(void)
     printf("Size of the Stack: %d\n", thread->stack_size );
     printf("Controll-Register: %#010x\n", __get_CONTROL());
 
-    uint32_t writesize = 64;
-    uint8_t *writepoint;
-    writepoint = thread->memory->start_address;
+
 	enable_unprivileged_mode();
-    for (uint32_t i=0; i<= writesize; i++) {
-    	*writepoint = 0xAA;
-    	writepoint++;
-    }
+
+/*	uint32_t *pointer = thread->memory->start_address + 50;
+	printf("Writing to Pointer: %#010x\n", pointer);
+	*pointer = 0xAAAA;
+	printf("Pointer Value: %#010x\n", *pointer);
+
+
+	printf("TCB Endaddress: %#010x\n\n", thread->memory->end_address);
+
+	uint32_t *pointer = thread->memory->start_address + 40;
+	int i = 0;
+	for (i=0; i < 1000; i++){
+	printf("Writing to Pointer: %#010x\n", pointer);
+	*pointer = 0xAAAAAAAA;
+	printf("Pointer Value: %#010x\n", *pointer);
+	pointer = pointer + 10;
+	}
+
+	pointer = thread->memory->start_address - 1;
+	printf("Writing to Pointer: %#010x\n", pointer);
+	*pointer = 0xAAAA;
+	printf("Pointer Value: %#010x\n", *pointer); */
 
     while(1);
 }
